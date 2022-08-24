a181930233905cc0a867484ace57b4097e38fb9b
mohammedboreny <Mohammed.boreny@gmail.com>
2022-08-24 23:04:32 +0300

    sign Up functionality Optimized

--- app.js
+++ app.js
@@ -9,25 +9,26 @@ class signUpForm  {
 
 
 
-function signUpButton(event){
+function signUpButton(el){
     let fName = document.getElementById("sfname").value;
     let lastNAme = document.getElementById("slname").value;
     let email = document.getElementById("semail").value;
     let password = document.getElementById("spass").value; 
     let Form = new signUpForm(fName, lastNAme, email, password);
     console.log(Form);
-    event.preventDefault();
+    el.preventDefault();
     localStorage.setItem("FormLocal", JSON.stringify(Form));
 };
 
-const form1 = documentgetElementById("sform");
+const form1 = document.getElementById("sform");
 // console.log(form);
 form1.addEventListener('submit', signUpButton);
 
 
 
 
-const form2
+const form2 = document.getElementById("lform");
+form2.addEventListener('submit', SignIn)
 function SignIn(params) {
     
 }
