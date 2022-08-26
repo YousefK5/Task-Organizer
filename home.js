9888a4cd94e7c574a7b18bb5ab0512521d85e272
ahmad-salman98 <ahmad98alsalman@gmail.com>
2022-08-26 23:41:15 +0300

    card style uncompleted

--- home.js
+++ home.js
@@ -25,3 +25,125 @@ function toggleTheme() {
 })();
 
 //End of set theme
+
+
+
+
+
+// ___________ cardsRow______________
+
+// which user profile is currently active ?
+let currentUserIndex = localStorage.getItem('currentUserIndex');
+
+// Call user's array list of saved tasks
+let usersArr = JSON.parse(localStorage.getItem('users'))
+let currentUser = usersArr[currentUserIndex];
+// print saved tasks 
+let userSavedTasks = currentUser.allTasks;
+printTask(userSavedTasks);
+
+// ______________________ FisrtName Welcome
+let name = document.getElementById('firstNameWlc').textContent = currentUser.fname;
+
+
+// constrctor  to make new task
+
+function newTask(taskTitle, taskDescription, taskPriority) {
+    this.taskTitle = taskTitle;
+    this.taskDescription = taskDescription;
+    this.taskPriority = taskPriority;
+}
+
+// add new task to array on submit
+let addTaskBtn = document.getElementById('addTaskBtn');
+
+addTaskBtn.addEventListener('click', subFunction);
+
+function subFunction(e) {
+    e.preventDefault();
+    let taskTitle = document.getElementById('taskTitle').value;
+    let taskDescription = document.getElementById('taskDescription').value;
+    let taskPriority = document.querySelector('input[name="priority"]:checked').value;
+    let task = new newTask(taskTitle, taskDescription, taskPriority);
+    userSavedTasks.push(task);
+    localStorage.setItem('users', JSON.stringify(usersArr));
+    addNewTask(task);
+
+}
+
+function printTask(arr) {
+    for (let i = 0; i < arr.length; i++)
+        addNewTask(arr[i])
+}
+
+
+function addNewTask(task) {
+
+    // define card layout 
+
+    let taskRow = document.getElementById('tasksRow');
+    let taskCard = document.createElement('div')
+
+    taskCard.classList.add('col-md-4', 'taskCard');
+    taskRow.appendChild(taskCard);
+
+    let title = document.createElement('h3');
+    taskCard.appendChild(title);
+
+    let taskBody = document.createElement('div');
+    taskCard.appendChild(taskBody);
+    taskBody.classList.add('taskBody');
+
+    let taskDescription = document.createElement('p');
+    taskBody.appendChild(taskDescription);
+
+    let doneRemoveDiv = document.createElement('div');
+    taskBody.appendChild(doneRemoveDiv);
+    doneRemoveDiv.classList.add('doneRemoveDiv');
+
+
+    let removeTaskBtn = document.createElement('button');
+    doneRemoveDiv.appendChild(removeTaskBtn);
+    removeTaskBtn.classList.add('removeTaskBtn');
+    removeTaskBtn.innerHTML = '<i class="fa-regular fa-trash-can fa-2xl"></i>'
+
+
+    let doneTaskBtn = document.createElement('button');
+    doneRemoveDiv.appendChild(doneTaskBtn);
+    doneTaskBtn.classList.add('doneTaskBtn');
+    doneTaskBtn.innerHTML = '<i class="fa-regular fa-circle-check fa-2xl"></i>'
+
+
+    let taskPriority = document.createElement('div');
+    taskCard.appendChild(taskPriority);
+    taskPriority.classList.add('priorityImgParent')
+
+
+    let priorityImg = document.createElement('img');
+    taskPriority.appendChild(priorityImg);
+
+
+    // define values
+    title.textContent = task.taskTitle;
+    taskDescription.textContent = task.taskDescription;
+
+    if (task.taskPriority == 'critical') {
+        taskPriority.classList.add('criticalPriority');
+        priorityImg.setAttribute('src', './img/high-priority.png');
+        title.style.backgroundColor = "#FD4848";
+    }
+    else if (task.taskPriority == 'normal') {
+        taskPriority.classList.add('meduimPriority');
+        priorityImg.setAttribute('src', './img/normal-priority.png');
+        title.style.backgroundColor = "#FFBA4A";
+
+    }
+    else {
+        taskPriority.classList.add('lowerPriority');
+        priorityImg.setAttribute('src', './img/low-priority.png');
+        title.style.backgroundColor = "#00c9af";
+
+    }
+}
+
+
--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 //Create Constructer for Users
-class User{
-    constructor(fname, lname, email, pass){
+class User {
+    constructor(fname, lname, email, pass) {
         this.fname = fname;
         this.lname = lname;
         this.email = email;
         this.pass = pass;
     }
+
+    // _________________________________________ Add array of saved tasks to each user//Ahmad
+
+    allTasks = [];
 };
 
 const form1 = document.getElementById("sform");
@@ -14,86 +18,89 @@ form1.addEventListener('submit', signUpButton);
 // Get users from local storage 
 let users;
 
-if(localStorage.getItem('users') != null) {
-    users= JSON.parse(localStorage.getItem('users'));
+if (localStorage.getItem('users') != null) {
+    users = JSON.parse(localStorage.getItem('users'));
 }
 else {
-    users=[];
+    users = [];
 }
 
 // check first name and last name that correct
 
-let fnameInput= document.getElementById("sfname");
-let lnameInput= document.getElementById("slname");
-let emailInput= document.getElementById("semail");
-let isfName=true;
-let islName=true;
+let fnameInput = document.getElementById("sfname");
+let lnameInput = document.getElementById("slname");
+let emailInput = document.getElementById("semail");
+let isfName = true;
+let islName = true;
 
 fnameInput.onblur = (e) => {
-    if(!(/^[a-z]+$/igm.test(fnameInput.value))) {
-        fnameInput.style.cssText= "border: 2px solid red"
-        isfName=false;
+    if (!(/^[a-z]+$/igm.test(fnameInput.value))) {
+        fnameInput.style.cssText = "border: 2px solid red"
+        isfName = false;
     }
     else {
-        fnameInput.style.cssText="border : none";
-        isfName=true;
+        fnameInput.style.cssText = "border : none";
+        isfName = true;
     }
 }
 
 lnameInput.onblur = (e) => {
-    if(!(/^[a-z]+$/igm.test(lnameInput.value))) {
-        lnameInput.style.cssText= "border: 2px solid red"
-        islName=false;
+    if (!(/^[a-z]+$/igm.test(lnameInput.value))) {
+        lnameInput.style.cssText = "border: 2px solid red"
+        islName = false;
     }
     else {
-        lnameInput.style.cssText="border : none";
-        islName=true;
+        lnameInput.style.cssText = "border : none";
+        islName = true;
     }
 }
 
-let subText= document.createElement("sub");
-let subEmail= document.createElement("sub");
+let subText = document.createElement("sub");
+let subEmail = document.createElement("sub");
 
 // add action in sign up button
 
-function signUpButton(el){
+function signUpButton(el) {
     el.preventDefault();
     let fName = document.getElementById("sfname").value;
     let lastNAme = document.getElementById("slname").value;
     let email = document.getElementById("semail").value;
-    let password = document.getElementById("spass").value; 
+    let password = document.getElementById("spass").value;
 
     //check email that regiestred
-    let isEmail=false;
+    let isEmail = false;
     users.forEach(e => {
-        if(email === e.email) {
-            isEmail=true;
+        if (email === e.email) {
+            isEmail = true;
         }
     })
     // to prevent wrong subtext reapet
-    subText.style.display="none";
-    subEmail.style.display="none";
+    subText.style.display = "none";
+    subEmail.style.display = "none";
     // if email isn't registered 
-    if(!isEmail) { 
-        if(isfName && islName) {
+    if (!isEmail) {
+        if (isfName && islName) {
             let user = new User(fName, lastNAme, email, password);
             users.push(user);
             localStorage.setItem(`users`, JSON.stringify(users));
+            // __________________________________________________ Add this user index to localStorage
+            localStorage.setItem('currentUserIndex', users.length - 1)
             form1.reset();
+
             location.replace("./home.html");
         }
         //print wrong input name subtext
-        else { 
-            subText.innerHTML ='First Name or Last Name is Wrong'
-            subText.style.cssText="color:red;margin:0 ; font-size:11px";
+        else {
+            subText.innerHTML = 'First Name or Last Name is Wrong'
+            subText.style.cssText = "color:red;margin:0 ; font-size:11px";
             lnameInput.after(subText);
         }
     }
     //print wrong input email subtext
     else {
-        emailInput.style.cssText= "border: 2px solid red"
-        subEmail.innerHTML ='Email is already regeistred'
-        subEmail.style.cssText="color:red;margin:0 ; font-size:11px";
+        emailInput.style.cssText = "border: 2px solid red"
+        subEmail.innerHTML = 'Email is already regeistred'
+        subEmail.style.cssText = "color:red;margin:0 ; font-size:11px";
         emailInput.after(subEmail);
     }
 };
@@ -108,30 +115,35 @@ function SignIn(el) {
     el.preventDefault();
     let email = document.getElementById('LEmail');
     let password = document.getElementById('LPass');
-    let isEmail=false;
-    let index=-1;
-    subEmail.style.display="none";
-    users.forEach((e,i) => {
-        if(email.value === e.email) {
-            isEmail=true;
-            index=i;
+    let isEmail = false;
+    let index = -1;
+    subEmail.style.display = "none";
+    users.forEach((e, i) => {
+        if (email.value === e.email) {
+            isEmail = true;
+            index = i;
         }
     })
-    if(isEmail) {
-        if(password.value == users[index].pass) {
+    if (isEmail) {
+        if (password.value == users[index].pass) {
+            // ___________________ Add the index of this user to local storage//Ahmad
+            localStorage.setItem('currentUserIndex', index)
             location.replace("./home.html");
+
         }
         else {
-            password.style.cssText= "border: 2px solid red"
-            subText.innerHTML ='Wrong Password'
-            subText.style.cssText="color:red;margin:0 ; font-size:13px";
+            password.style.cssText = "border: 2px solid red"
+            subText.innerHTML = 'Wrong Password'
+            subText.style.cssText = "color:red;margin:0 ; font-size:13px";
             password.after(subText);
         }
     }
     else {
-        email.style.cssText= "border: 2px solid red"
-        subEmail.innerHTML ='Email is not found, Sign Up please'
-        subEmail.style.cssText="color:red; margin:0 ; font-size:13px";
+        email.style.cssText = "border: 2px solid red"
+        subEmail.innerHTML = 'Email is not found, Sign Up please'
+        subEmail.style.cssText = "color:red; margin:0 ; font-size:13px";
         email.after(subEmail);
     }
 }
+
+
